e64bbf1620f6eae2420ad084b928f7030c7cdc26
Ertuğrul Pişkin <iwannabespace@Ertugrul-MacBook-Air.local>
2021-11-18 00:15:41 +0300

    include

---
+++ include/flags.h
@@ -0,0 +1,12 @@
+#ifndef _flags_h_
+#define _flags_h_
+
+typedef enum e_flags 
+{
+    NO_ERROR,
+    INVALID_TYPE_ERROR,
+    INVALID_VALUE_ERROR,
+    INSUFFICIENT_COMMAND_ERROR,
+} c_flags;
+
+#endif
